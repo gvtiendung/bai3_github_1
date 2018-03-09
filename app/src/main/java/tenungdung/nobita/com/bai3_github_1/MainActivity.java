00f629c6db06197b190545c0ababf188290694b5
gvtiendung <gvtiendung@gmail.com>
2018-03-09 13:56:47 +0700

    bai3_github_1 upload

---
+++ app/src/main/java/tenungdung/nobita/com/bai3_github_1/MainActivity.java
@@ -0,0 +1,13 @@
+package tenungdung.nobita.com.bai3_github_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
